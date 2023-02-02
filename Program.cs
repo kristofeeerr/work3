a423c5291a1400dc4532daf60270d98372f621e3
kristofeeerr <123837931+kristofeeerr@users.noreply.github.com>
2023-02-03 01:16:33 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameShop;
+            short rentalAmount;
+            object purchaseAmount;
+            int numberCoins;
+            byte minimumDiscount;
+            uint maximumDiscount;
+            char currency = '$';
+            bool haveEnoughCoins = true;
+            double bunPrice = 45.80;
+            float bunWeight = 66.9f;
+        }
+    }
+}
